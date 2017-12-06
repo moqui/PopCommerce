977220b50841e25598c0716dea3293e9622fde72
David E Jones <dej@dejc.com>
2017-12-06 00:56:14 -0800

    Updated for changes in data produced by mantle-usl tests

--- src/test/groovy/PopcAdminScreenTests.groovy
+++ src/test/groovy/PopcAdminScreenTests.groovy
@@ -148,9 +148,9 @@ class PopcAdminScreenTests extends Specification {
 
         // Accounting/Payment
         "Accounting/Payment/EditPayment?paymentId=55400" |
-                ['ORG_ZIZI_RETAIL', 'Applied $23,830.00', 'Current: Delivered']
+                ['ORG_ZIZI_RETAIL', 'Applied $24,000.00', 'Current: Delivered']
         "Accounting/Payment/PaymentCheck?paymentIds=55400&renderMode=xsl-fo" | ['Ziddleman',
-                'Twenty three thousand eight hundred thirty and 00/100', 'Picker Bot 2000']
+                'Twenty four thousand and 00/100', 'Picker Bot 2000']
         "Accounting/Payment/PaymentDetail?paymentIds=55400&renderMode=xsl-fo" |
                 ['Ziddleman', '$23,830.00', 'Picker Bot 2000']
 
